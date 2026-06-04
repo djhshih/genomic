4f39f87564436b1aeeca076fd0b500283bf394b3
David J. H. Shih <djh.shih@gmail.com>
2026-06-05 00:38:43 +0800

    Re-organize files.

--- tests/genomic_test.cpp
+++ tests/genomic_test.cpp
@@ -3,8 +3,8 @@
 #include <boost/test/unit_test.hpp>
 
 #include "config.h"
-#include "../global.hpp"
-#include "../SampleSets.hpp"
+#include "global.hpp"
+#include "SampleSets.hpp"
 #include "FilesDiff.hpp"
 
 #include <fstream>
