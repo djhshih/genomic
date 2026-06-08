c4ccd9476f5e373f80d340aed639db1b82c87b65
David J. H. Shih <djh.shih@gmail.com>
2026-06-08 23:43:20 +0800

    Continue cna namespace.

--- lib/GenericSampleSet.cpp
+++ lib/GenericSampleSet.cpp
@@ -8,16 +8,16 @@ void cna::GenericSampleSet::_read(std::fstream& file)
 	std::string ext = cna::name::fileext(fileName);
 	switch (cna::mapping::extension[ext]) {
 		case cna::data::raw:
-			rep = new RawSampleSet<cnvalue>(markers);
+			rep = new cna::RawSampleSet<cnvalue>(markers);
 			break;
 		case cna::data::raw_ascn:
-			rep = new RawSampleSet<alleles_cn>(markers);
+			rep = new cna::RawSampleSet<alleles_cn>(markers);
 			break;
 		case cna::data::segmented:
-			rep = new SegmentedSampleSet<cnvalue>(markers);
+			rep = new cna::SegmentedSampleSet<cnvalue>(markers);
 			break;
 		case cna::data::segmented_ascn:
-			rep = new SegmentedSampleSet<alleles_cn>(markers);
+			rep = new cna::SegmentedSampleSet<alleles_cn>(markers);
 			break;
 		default:
 			throw invalid_conversion("Cannot determine file type from file name extension.");
@@ -34,7 +34,7 @@ void cna::GenericSampleSet::_write(std::fstream& file)
 	// cast $rep to appropriate type
 	// runtime checking is skipped (i.e. use static_cast instead of dynamic_cast)
 	//  since exact type can be determined
-	SampleSet* tmp = NULL;
+	cna::SampleSet* tmp = NULL;
 	cna::data::Type outputType = cna::mapping::extension[ext];
 
 	if (outputType != rep->type()) {
@@ -43,7 +43,7 @@ void cna::GenericSampleSet::_write(std::fstream& file)
 			case cna::data::raw:
 				switch (rep->type()) {
 					case cna::data::segmented:
-						tmp = new RawSampleSet<cnvalue>(*static_cast<SegmentedSampleSet<cnvalue>*>(rep));
+						tmp = new cna::RawSampleSet<cnvalue>(*static_cast<cna::SegmentedSampleSet<cnvalue>*>(rep));
 						std::swap(rep, tmp);
 						delete tmp;
 						break;
@@ -54,7 +54,7 @@ void cna::GenericSampleSet::_write(std::fstream& file)
 			case cna::data::raw_ascn:
 				switch (rep->type()) {
 					case cna::data::segmented_ascn:
-						tmp = new RawSampleSet<alleles_cn>(*static_cast<SegmentedSampleSet<alleles_cn>*>(rep));
+						tmp = new cna::RawSampleSet<alleles_cn>(*static_cast<cna::SegmentedSampleSet<alleles_cn>*>(rep));
 						std::swap(rep, tmp);
 						delete tmp;
 						break;
@@ -65,7 +65,7 @@ void cna::GenericSampleSet::_write(std::fstream& file)
 			case cna::data::segmented:
 				switch (rep->type()) {
 					case cna::data::raw:
-						tmp = new SegmentedSampleSet<cnvalue>(*static_cast<RawSampleSet<cnvalue>*>(rep));
+						tmp = new cna::SegmentedSampleSet<cnvalue>(*static_cast<cna::RawSampleSet<cnvalue>*>(rep));
 						std::swap(rep, tmp);
 						delete tmp;
 						break;
@@ -76,7 +76,7 @@ void cna::GenericSampleSet::_write(std::fstream& file)
 			case cna::data::segmented_ascn:
 				switch (rep->type()) {
 					case cna::data::raw_ascn:
-						tmp = new SegmentedSampleSet<alleles_cn>(*static_cast<RawSampleSet<alleles_cn>*>(rep));
+						tmp = new cna::SegmentedSampleSet<alleles_cn>(*static_cast<cna::RawSampleSet<alleles_cn>*>(rep));
 						std::swap(rep, tmp);
 						delete tmp;
 						break;
